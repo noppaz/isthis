b7c9d61c78f6a710760d60800248157be514f900
Noah Holm <32292420+noppaz@users.noreply.github.com>
2025-04-26 12:12:06 +0200

    refactor playlist creation

--- isthis.py
+++ isthis.py
@@ -1,6 +1,5 @@
 import configparser
 from dataclasses import dataclass
-from typing import List, Tuple
 
 import click
 import spotipy
@@ -17,7 +16,7 @@ class Track:
     popularity: int
 
 
-def authorize() -> Tuple[spotipy.Spotify, str, str]:
+def authorize() -> tuple[spotipy.Spotify, str, str]:
     config = configparser.ConfigParser()
     config.read("settings.conf")
 
@@ -42,7 +41,7 @@ def authorize() -> Tuple[spotipy.Spotify, str, str]:
 
 def get_artist_tracks_uris(
     sp: spotipy.Spotify, artist: str, market: str
-) -> Tuple[str, List[str]]:
+) -> tuple[str, list[str]]:
     artist_albums = sp.artist_albums(artist, country=market, limit=50)
     artist_name = get_artist_name(artist, artist_albums["items"])
     print("Searching for tracks by", artist_name)
@@ -58,7 +57,7 @@ def get_artist_tracks_uris(
     return artist_name, artist_tracks
 
 
-def get_artist_name(artist: str, artist_albums: List[dict]) -> str:
+def get_artist_name(artist: str, artist_albums: list[dict]) -> str:
     for album in artist_albums:
         for a in album["artists"]:
             if a["uri"] == artist:
@@ -67,21 +66,16 @@ def get_artist_name(artist: str, artist_albums: List[dict]) -> str:
 
 
 def get_track_popularity(
-    sp: spotipy.Spotify, market: str, artist_tracks: list
-) -> List[Track]:
-    sorted_tracks = []
-    i = 0
-    while i < len(artist_tracks):
-        tracks = sp.tracks(artist_tracks[i : 50 + i], market=market)  # noqa: E203
-        for track in tracks["tracks"]:
-            t = Track(track["name"], track["uri"], track["popularity"])
-            sorted_tracks.append(t)
-        i += 50
-    sorted_tracks = sorted(
-        sorted_tracks, key=lambda track: track.popularity, reverse=True
-    )
-
-    return sorted_tracks
+    sp: spotipy.Spotify, market: str, track_uris: list
+) -> list[Track]:
+    artist_tracks: list[Track] = []
+    for i in range(0, len(track_uris), 50):
+        tracks = sp.tracks(track_uris[i : i + 50], market=market)  # noqa: E203
+        artist_tracks.extend(
+            Track(track["name"], track["uri"], track["popularity"])
+            for track in tracks["tracks"]
+        )
+    return sorted(artist_tracks, key=lambda track: track.popularity, reverse=True)
 
 
 def create_playlist(
@@ -91,16 +85,17 @@ def create_playlist(
     username: str,
     number_of_tracks: int,
 ) -> None:
-    track_uris = [sortedTrack.uri for sortedTrack in sorted_tracks]
-    tracks_to_add = track_uris[:number_of_tracks]
+    selected_track_uris = [
+        sortedTrack.uri for sortedTrack in sorted_tracks[:number_of_tracks]
+    ]
     description = (
         "This is playlist equivalent. Generated with https://github.com/noppaz/isthis"
     )
     pl = sp.user_playlist_create(
         username, f"Is This {artist_name}", public=True, description=description
     )
-    sp.user_playlist_add_tracks(username, pl["uri"], tracks_to_add)
-    print(f"Playlist created and {len(tracks_to_add)} songs added")
+    sp.user_playlist_add_tracks(username, pl["uri"], selected_track_uris)
+    print(f"Playlist created and {len(selected_track_uris)} songs added")
 
 
 def search_artists(sp: spotipy.Spotify, query: str) -> list:
